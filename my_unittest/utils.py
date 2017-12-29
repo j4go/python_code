a570789b764be298644cde1b1c837f9139ef7914
lgf17 <yianny@163.com>
2017-12-29 08:39:24 +0800

    添加https://github.com/dkua/talks.git示例demo

--- my_unittest/utils.py
+++ my_unittest/utils.py
@@ -1,3 +1,10 @@
+"""
+Test example
+code for testing
+
+Testing is important
+Testing is hard: code/isolation/fixtures/indirect value
+"""
 
 def to_str(data):
     """Tansform data to a str"""
@@ -10,5 +17,33 @@ def to_str(data):
                         'found: %r' % data)
 
 
-# def test_to_str():
-#     assert to_str('hello') == 'hello'
+def test_to_str():
+    assert to_str('hello') == 'hello'
+
+
+def encode(input_string):
+    if not input_string:
+        return []
+    count = 1
+    prev = ''
+    lst = []
+    for character in input_string:
+        if character != prev:
+            if prev:
+                entry = (prev, count)
+                lst.append(entry)
+            count = 1
+            prev = character
+        else:
+            count += 1
+    else:
+        entry = (character, count)
+        lst.append(entry)
+    return lst
+
+
+def decode(lst):
+    q = ''
+    for character, count in lst:
+        q += character * count
+    return q
